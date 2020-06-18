49e27c658ba865b97e030364bf7ee434867053de
Ubuntu <gopalharichanda@gopalharichanda.qgcpbsuehh4ezahstnc1jv3vmg.gx.internal.cloudapp.net>
2020-06-18 19:05:44 +0000

    Second Commit

---
+++ src/main/java/com/mindtree/devopsProj/NewClass.java
@@ -0,0 +1,12 @@
+package com.mindtree.devopsProj;
+
+import org.junit.Test;
+
+public class NewClass {
+	@Test
+	public void printMessage() {
+
+		System.out.println("Printing from New Class");
+
+	}
+}
